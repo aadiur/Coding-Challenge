12dd07bca1261d5098d29f52aabdbf63fba6d5e0
aadiur <aadiur710@gmail.com>
2026-07-17 23:38:57 +0530

    Time: 8 ms (83.07%), Space: 11.9 MB (54.18%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,13 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-    unordered_map<ListNode*, bool> mp;
-    ListNode*temp=head;
-    while(temp) {
-        if(mp.find(temp) != mp.end()) {
-            return true;
-        }
-        mp[temp] = true;
-        temp = temp->next;
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast&&fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)return true;
+        }return false;
+        
     }
-    return false;}
 };
